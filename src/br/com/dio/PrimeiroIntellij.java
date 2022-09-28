ccc4c6b3d4c399cd9cb0555e8e5ecc697861527c
Marcelo Jeronymo <jeronymos@hotmail.com>
2022-09-28 16:23:51 -0300

    First Commit

---
+++ src/br/com/dio/PrimeiroIntellij.java
@@ -0,0 +1,18 @@
+package br.com.dio;
+
+import br.com.dio.model.Programa1;
+
+public class PrimeiroIntellij {
+    public static void main(String[] args) {
+
+        Programa1 programa1 = new Programa1();
+
+        System.out.println(programa1);
+
+        /*int a = 5;
+        int b = 3;
+        System.out.println("Hello Marcelo!" + (a+b));*/
+
+
+    }
+}
---
+++ src/br/com/dio/model/Programa1.java
@@ -0,0 +1,65 @@
+package br.com.dio.model;
+
+import java.util.Objects;
+
+public class Programa1 {
+
+    private String nome;
+    private String cor;
+    private Integer idade;
+
+     public Programa1() {}
+
+    public Programa1(String nome, String cor, Integer idade) {
+        this.nome = nome;
+        this.cor = cor;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Programa1 programa1 = (Programa1) o;
+        return Objects.equals(nome, programa1.nome) && Objects.equals(cor, programa1.cor) && Objects.equals(idade, programa1.idade);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, cor, idade);
+    }
+
+    @Override
+    public String toString() {
+        return "Programa1{" +
+                "nome='" + nome + '\'' +
+                ", cor='" + cor + '\'' +
+                ", idade=" + idade +
+                '}';
+    }
+
+}
